5cdbb7f6a03be944e68076516b9f0816eae59415
faaizmohammad5-svg <faaizmohammad5@gmail.com>
2026-09-09 11:34:42 +0530

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+app = Flask(__name__)
+
+    app.register_blueprint(
+        youtube_bp,
+        url_prefix="/youtube"
+    )
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/html")
+    def html():
+        return render_template("index.html")
+
+    return app
